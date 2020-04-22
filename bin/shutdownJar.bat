ec012b6f52e1d91efd0e7d7b16c52d7a165fe0f4
lanhaifeng <lanhaifeng@mchz.com.cn>
2020-04-22 18:14:08 +0800

    修改脚本

--- bin/shutdownJar.bat
+++ bin/shutdownJar.bat
@@ -1,3 +1,7 @@
 @echo off
 taskkill -f -t -im javaw.exe
-exit
+
+@rem kill last java.exe thread
+@rem for /f "tokens=2" %%x in ('tasklist ^| findstr java.exe') do set PIDTOKILL=%%x
+@rem for /f "tokens=2" %%x in ('tasklist ^| findstr java.exe ^| findstr %PIDTOKILL%') do taskkill /F /PID %%x
+pause
